83c55f8bccc906e202d6f68a8c118c12aa933061
RogerioNeto07 <joserogeriofonsecauzl@gmail.com>
2025-11-23 19:32:06 -0300

    feat: initialize project structure with layered architecture

---
+++ src/main/java/rogerio/n/escolar/edu/br/Catalogo/CFStyle/CatalogoCfStyleApplication.java
@@ -0,0 +1,13 @@
+package rogerio.n.escolar.edu.br.Catalogo.CFStyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoCfStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoCfStyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rogerio/n/escolar/edu/br/Catalogo/CFStyle/CatalogoCfStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package rogerio.n.escolar.edu.br.Catalogo.CFStyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoCfStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
